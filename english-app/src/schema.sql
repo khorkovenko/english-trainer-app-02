dccd1892877d7bd7a63202e076f6d92bce05254a
artificialLeo <horkovenko.k@gmail.com>
2025-08-11 23:19:14 +0300

    add new tables

--- english-app/src/schema.sql
+++ english-app/src/schema.sql
@@ -16,3 +16,20 @@ create table grammar (
                          created_at timestamp with time zone default timezone('utc', now()),
                          updated_at timestamp with time zone default timezone('utc', now())
 );
+
+create table reading (
+                         id uuid primary key default uuid_generate_v4(),
+                         user_id uuid references auth.users(id) on delete cascade,
+                         theme text not null,
+                         prompt text not null,
+                         created_at timestamp with time zone default now(),
+                         updated_at timestamp with time zone default now()
+);
+
+create table reading_prompts (
+                                 id uuid primary key default uuid_generate_v4(),
+                                 reading_id uuid references reading(id) on delete cascade,
+                                 prompt text not null,
+                                 created_at timestamp with time zone default now(),
+                                 updated_at timestamp with time zone default now()
+);
